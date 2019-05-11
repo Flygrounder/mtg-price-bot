27b1385a92fefb3e89c3f8e22e4b6d75c7709cd7
Artyom Belousov <flygrounder@yandex.ru>
2019-05-11 20:59:11 +0300

    Refactoring

--- cardsinfo/names.go
+++ cardsinfo/names.go
@@ -5,27 +5,6 @@ import (
 	"strings"
 )
 
-func getCardName(card *mtg.Card) string {
-	switch card.Layout {
-	case "split":
-		return strings.Join(card.Names, " // ")
-	case "transform":
-		return strings.Join(card.Names, " | ")
-	default:
-		return card.Name
-	}
-}
-
-func getOriginalNameFromLang(name, lang string, channel chan string) {
-	cards, _, _ := mtg.NewQuery().Where(mtg.CardLanguage, lang).Where(mtg.CardName, name).PageS(1, 1)
-	if len(cards) > 0 {
-		name := getCardName(cards[0])
-		channel <- name
-	} else {
-		channel <- ""
-	}
-}
-
 func GetOriginalName(name string) string {
 	langs := []string{"Russian", ""}
 	channel := make(chan string)
@@ -39,4 +18,25 @@ func GetOriginalName(name string) string {
 		}
 	}
 	return ""
+}
+
+func getOriginalNameFromLang(name, lang string, channel chan string) {
+	cards, _, _ := mtg.NewQuery().Where(mtg.CardLanguage, lang).Where(mtg.CardName, name).PageS(1, 1)
+	if len(cards) > 0 {
+		name := getCardName(cards[0])
+		channel <- name
+	} else {
+		channel <- ""
+	}
+}
+
+func getCardName(card *mtg.Card) string {
+	switch card.Layout {
+	case "split":
+		return strings.Join(card.Names, " // ")
+	case "transform":
+		return strings.Join(card.Names, " | ")
+	default:
+		return card.Name
+	}
 }
