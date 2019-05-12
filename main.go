002c175af1a456c17906e682053a31dfd56d51a1
Artyom Belousov <flygrounder@yandex.ru>
2019-05-12 11:34:16 +0300

    Removed logger

--- main.go
+++ main.go
@@ -4,17 +4,10 @@ import (
 	"github.com/flygrounder/mtg-price-vk/vk"
 	"github.com/gin-gonic/gin"
 	"math/rand"
-	"runtime"
 	"time"
 )
 
 func main() {
-	go (func() {
-		for {
-			time.Sleep(5 * time.Second)
-			println(runtime.NumGoroutine())
-		}
-	})()
 	rand.Seed(time.Now().UTC().UnixNano())
 	r := gin.Default()
 	r.POST("callback/message", vk.HandleMessage)
